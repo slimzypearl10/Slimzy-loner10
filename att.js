b5b55d16be2399aae763fd1e6194f44ef5919f6d
slimzyloner10 <daveelda10@gmail.com>
2024-11-19 12:23:57 +0100

    first commit

---
+++ att.js
@@ -0,0 +1,41 @@
+//   const images = [
+//       'image-1',    
+//       'image-2',
+//       'image-3',
+//       'image-4',
+//       'image-5',
+//       'image-6',
+//       'image-7',
+//       'image-8',
+//       'image-9',
+//       'image-10',
+//       'image-11',
+//       'image-12',
+//   ];
+
+//   const flowingImages = document.getElementById('flowing-images');
+
+//     Create and append images to the container
+//   images.forEach(src => {
+//       const img = document.createElement('img');
+//       img.src = src;
+//       img.alt = 'flowing-images';
+//       flowingImages.appendChild(img);
+//   });
+
+
+//     Clone the images for seamless flow
+//   images.forEach(src => {
+//       const img = document.createElement('img');
+//       img.src = src;
+//       img.alt = 'flowing-images';
+//     flowingImages.appendChild(img);
+//  });
+
+ 
+
+
+
+
+ 
+ 
